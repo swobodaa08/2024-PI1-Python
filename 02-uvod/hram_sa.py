0506bae64d173b10e20d537dccfc19787cd16bda
Samuel Svoboda <svobodas2008@gmail.com>
2024-10-04 22:28:08 +0200

    hram sa doma

---
+++ 02-uvod/hram_sa.py
@@ -0,0 +1,7 @@
+rok = int(input("What year is your birthday?: "))
+vek = 2024-rok
+
+if vek > 18:
+    print("You are", vek, "years old.")
+else:
+    print("Whoops, unfortunately, you have to be atleast 18 years old to access this website...")
