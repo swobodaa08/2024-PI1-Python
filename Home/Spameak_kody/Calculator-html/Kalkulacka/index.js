45aeb12216c489b6da90d5888e56e1012b99767b
Samuel Svoboda <svobodas2008@gmail.com>
2025-02-12 09:54:49 +0100

    fix

--- Home/Spameak_kody/Calculator-html/Kalkulacka/index.js
+++ Home/Spameak_kody/Calculator-html/Kalkulacka/index.js
@@ -24,8 +24,3 @@ function calculate(){
 function lastResult(){
     display.value = last_result
 }
-
-function mocnina(input){
-    display.value += input;
-    display.value = eval(display.value);
-}
