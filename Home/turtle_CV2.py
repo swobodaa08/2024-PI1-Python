b59985c7b41343b0894f72e96b5a7d08d2a5d9f9
Samuel Svoboda <svobodas2008@gmail.com>
2025-02-14 13:09:01 +0100

    flame

--- Home/turtle_CV2.py
+++ Home/turtle_CV2.py
@@ -1,20 +1,38 @@
 import turtle
-from tkinter import Entry
+import tkinter as tk
 
 t = turtle.Turtle()
+root = tk.Tk()
+root.geometry("500x500")
+farba1 = "blue"
+farba2 = "yellow"
 
-def terc(pocet):
-    first = 15
-    t.pencolor("yellow")
-    t.dot(first)
+def terc(pocet, farba1, farba2):
+    biggest = (pocet * 15) + 15
     for _ in range(pocet):
-        t.pencolor("yellow")
-        first = first + 15
-        t.dot(first)
+        biggest = biggest - 15
+        t.pencolor(farba1)
+        t.dot(biggest)
+        biggest = biggest - 15
+        print(biggest)
+        t.pencolor(farba2)
+        t.dot(biggest)
+        if biggest == 15:
+            break
 
-entryy = Entry
+def prekresli():
+    farba1 = entry1.__getattribute__()
+    farba2 = entry2.__getattribute__()
+    farba1 = str
+    farba2 = str
+    terc(20, farba1, farba2)
 
+terc(20, farba1, farba2)
 
-terc(20)
+label = tk.Label()
+entry1 = tk.Entry(root).pack()
+entry2 = tk.Entry(root).pack()
+button = tk.Button(root, text="Prekresli", command=prekresli).pack()
+
 
 turtle.exitonclick()
