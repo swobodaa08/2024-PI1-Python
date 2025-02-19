6caa44384ded2179c6f8b314c3a3ab44edf1c458
Samuel Svoboda <svobodas2008@gmail.com>
2025-02-19 10:02:00 +0100

    nic

--- 13-udalosti/logo-maker.py
+++ 13-udalosti/logo-maker.py
@@ -9,27 +9,32 @@ screen_height = root.winfo_screenheight()
 root.attributes("-fullscreen", True)
 root.title("LogoMaker - Svoboda")
 root.geometry(f"{screen_width}x{screen_height}")
-canvas = tk.Canvas(root, width=1920, height=1080)
+canvas = tk.Canvas(root)
 
-def nakresli(event):
-    radius = velkosť.get()
+def vykresli(event):
+    radius = size.get()
     x1 = event.x - radius
     y1 = event.y - radius
     x2 = event.x + radius
     y2 = event.y + radius
-
-    canvas.create_oval(x1, y1, x2, y2)
-
-def circle():
-    canvas.bind("<ButtonPress>", nakresli)
+    canvas.create_oval(x1, y1, x2, y2, fill="black")
 
 def square():
     pass
 
 
-kruh = tk.Button(text="O", command=circle).pack()
-stvorec = tk.Button(text="D", command=square).pack()
-velkosť = tk.Scale(root, orient="horizontal", from_=5, to=40, length=140).pack()
+kruh = tk.Button(text="O", font=("30"))
+kruh.bind("<ButtonPress>", vykresli)
+kruh.place(x=20, y=10)
+kruh.pack()
+
+stvorec = tk.Button(text="D", command=square)
+stvorec.place(x=40, y=10)
+stvorec.pack()
+
+size = tk.Scale(root, orient="horizontal", from_=5, to=40, length=140)
+size.place(x=90, y=100)
+size.pack()
 
 
 
