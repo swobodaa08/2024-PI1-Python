6b90cfaa6b80711a6e891fc987e7ea54ec2c4c09
Samuel Svoboda <svobodas2008@gmail.com>
2025-02-26 20:12:32 +0100

    uloha

--- 14-string/07-shifra.py
+++ 14-string/07-shifra.py
@@ -1,5 +1,28 @@
+import random
+
+list = [-4, -3, -2, -1, 1, 2, 3, 4]
+
 while True:
+    cislo = random.choice(list)
     text = input("Zadaj input: ")
-    for i in range(len(text)):
-        i += 1
-        print(ord(text[i]))
+    zasifrovane = ""
+    
+    for i in text:
+        zasifrovane += chr(ord(i) + cislo)
+    
+    print(zasifrovane)
+    if cislo > 0:
+        print(f"Zašifrovali sme každý znak o {cislo} znaky dopredu")
+    else:
+        print(f"Zašifrovali sme každý znak o {cislo} znaky dozadu")
+
+# print(ord("d"))
+# print(ord("u"))
+# print(ord("s"))
+# print(ord("a"))
+# print(ord("n"))
+# print(ord("e"))
+# print(ord("v"))
+# print(ord("t"))
+# print(ord("b"))
+# print(ord("o"))
