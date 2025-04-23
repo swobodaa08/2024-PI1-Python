656c46c2c4e1fbec31b266ebb0a80529fc3f9065
Samuel Svoboda <svobodas2008@gmail.com>
2025-04-23 10:01:49 +0200

    nic

--- 18-txt/du.py
+++ 18-txt/du.py
@@ -5,10 +5,5 @@ canvas.pack()
 
 
 fbody = open("18-txt/body.txt", "r")
-for body in fbody:
-    # print(body)
-    x, y = (body.split(" "))
-    x = int(x)
-    y = int(y)
-    canvas.create_oval(x-5, y-5, x+5, y+5)
+
 tk.mainloop()
