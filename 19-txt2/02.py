8312ac110d1eda60e6ac290ea813d8170452e56a
Samuel Svoboda <svobodas2008@gmail.com>
2025-05-07 08:03:45 +0200

    nic

--- 19-txt2/02.py
+++ 19-txt2/02.py
@@ -3,19 +3,25 @@ import tkinter as tk, random
 fdraw = open("19-txt2/tvary.txt", "r", encoding="UTF-8")
 
 canvas = tk.Canvas(width=800, height=800)
+canvas.pack()
+
+riadok = fdraw.readline()
+print(riadok)
+tvar, x1, y1, x2, y2, farba = riadok.split()
+print(tvar)
+print(x1)
+print(y1)
+print(x2)
+print(y2)
+print(farba)
+
+if tvar == "oval":
+    canvas.create_oval(x1, y1, x2, y2, fill=farba)
+elif tvar == "rectangle":
+    canvas.create_rectangle(x1, y1, x2, y2, fill=farba)
 
 def draw():
-    riadok = fdraw.readline()
-    print(riadok)
-    tvar, x1, y1, x2, y2, farba = riadok.split()
-    print(tvar)
-    print(x1)
-    print(y1)
-    print(x2)
-    print(y2)
-    print(farba)
-    canvas.create_{tvar}()
-
+    pass
 draw()
 
 fdraw.close()
